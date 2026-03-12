jtschuster/llvm-project

commit 913c5b4d1fffa00585edb6ba4f0915f29afa90cc

clang -cc1as: Remove a redundant initSections call
`Parser->Run(Opts.NoInitialTextSection)` calls initSections. Remove a
redundant initSections to remove an extra FT_Align fragment, observed
when investigating a missing MCOrgFragment relaxation issue
https://github.com/ClangBuiltLinux/linux/issues/2116

clang/tools/driver/cc1as_main.cpp
@@ -577,7 +577,6 @@ static bool ExecuteAssemblerImpl(AssemblerInvocation &Opts,
     Triple T(Opts.Triple);
     Str.reset(TheTarget->createMCObjectStreamer(
         T, Ctx, std::move(MAB), std::move(OW), std::move(CE), *STI));
-    Str->initSections(Opts.NoExecStack, *STI);
     if (T.isOSBinFormatMachO() && T.isOSDarwin()) {
       Triple *TVT = Opts.DarwinTargetVariantTriple
                         ? &*Opts.DarwinTargetVariantTriple

commit c14a9fd41ce51c02f24024d80f879ca127a7ab6a

Add test for -cc1as -mnoexecstack emitting .note.GNU-stack
Verify that -mnoexecstack actually produces a .note.GNU-stack section in the output object file, not just that the flag is forwarded by the driver. This would have caught the regression in 913c5b4d1fff.

clang/test/Misc/cc1as-noexecstack.s
@@ -1,0 +1,10 @@
+// REQUIRES: x86-registered-target
+// RUN: %clang -cc1as -triple x86_64-linux-gnu -filetype obj -mnoexecstack -o %t.o %s
+// RUN: llvm-readelf -S %t.o | FileCheck %s
+
+// CHECK: .note.GNU-stack
+
+.text
+.globl foo
+foo:
+    retq